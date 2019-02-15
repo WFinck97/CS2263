34dd6656862cc81146c0a97d6b0a0cb152a096f9
Wesley Finck <wesleyfinck@gmail.com>
2019-02-15 18:11:59 -0400

    added header files

---
+++ A3/mainq1.c
@@ -0,0 +1,6 @@
+// mainq1.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Word_Stats.h"
---
+++ A3/mainq2.c
@@ -0,0 +1,6 @@
+// mainq2.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Word_Stats.h"
