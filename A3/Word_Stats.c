97cb3a53d9aefbcdcb989bf879ffb712bebb220d
Wesley Finck <wesleyfinck@gmail.com>
2019-02-17 08:39:35 -0400

    will check if word is in line

--- A3/Word_Stats.c
+++ A3/Word_Stats.c
@@ -6,4 +6,9 @@ void getWordStats(char *word, FILE *f, int *sum, int *num){
 	/* look through each line for 'word', only need to look for the first instance
 	of the word. If found, add score to sum and num++. move to next line.
 	*/
+	char *line;
+
+	if(strstr(line, word)){
+
+	}
 }
