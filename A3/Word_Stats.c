bad98dd3edbc6f3800b6d487e3aa5a292eae7f2e
Wesley Finck <wesleyfinck@gmail.com>
2019-02-17 08:30:56 -0400

    added a description to Word_Stats.c

--- A3/Word_Stats.c
+++ A3/Word_Stats.c
@@ -3,5 +3,7 @@
 #include "Word_Stats.h"
 
 void getWordStats(char *word, FILE *f, int *sum, int *num){
-	
+	/* look through each line for 'word', only need to look for the first instance
+	of the word. If found, add score to sum and num++. move to next line.
+	*/
 }
