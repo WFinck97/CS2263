a8330b20e8433d001a51a047179f24fb8d2f67e9
Wesley Finck <wesleyfinck@gmail.com>
2019-02-18 14:20:41 -0400

    started working on mainq2.c

--- A3/mainq2.c
+++ A3/mainq2.c
@@ -4,3 +4,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Word_Stats.h"
+
+#define MAX_REVIEW_L 200
+
+int main(int argc, char* argv[]){
+	
+	char const* const fileName = argv[1]; /* should check that argc > 1 */
+    FILE* file = fopen(fileName, "r"); /* should check the result */
+
+    char review[MAX_REVIEW_L], cin;
+    int *psum, *pnum, num = 0, sum = 0, i = 0;
+    psum = &sum;
+    pnum = &num;
+
+    printf("enter a review: ");
+    while(cin != '\n'){
+    	scanf("%c", &cin);
+    	review[i] = cin;
+    	i++;
+    }
+
+    review[i-1] = '\0';
+
+    printf("%s\n", review);
+    //getWordStats(word, file, psum, pnum);
+    printf("the sum is: %d\n", sum);
+    //printf("%s appeared %d times\n", word, num);
+    //printf("The average score for reviews containing %s is %lf\n", word, (double) sum/num);
+    fclose(file);
+
+    return 0;
+
+}
