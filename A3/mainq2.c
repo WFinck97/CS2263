6241b4ad0c2c6e349526e4ea3d12148cc825b5ae
Wesley Finck <wesleyfinck@gmail.com>
2019-02-18 18:08:07 -0400

    adds up average for a review, but slighly off from manual

--- A3/mainq2.c
+++ A3/mainq2.c
@@ -24,11 +24,12 @@ int main(int argc, char* argv[]){
     	return EXIT_FAILURE;
     }
 
-    int *psum, *pnum, num = 0, sum = 0;
+    double revSum = 0, avg;
+    int *psum, *pnum, num = 0, sum = 0, numWord = 0;
     psum = &sum;
     pnum = &num;
 
-    printf("enter a review: ");
+    printf("enter a review:\n");
     
     char line[LINEL+1], word[WORDL+1];
     fgets(line, LINEL+1, stdin);
@@ -45,16 +46,32 @@ int main(int argc, char* argv[]){
         getWordStats(word, file, psum, pnum);
 	    printf("the sum is: %d\n", sum);
 	    printf("%s appeared %d times\n", word, num);
-	    printf("The average score for reviews containing %s is %lf\n", word, (double) sum/num);
+	    if(num){
+	    	avg = (double) sum / (double) num;
+	    	numWord++;
+	    }
+	    else{
+	    	avg = 0;
+	    }
+	    printf("The average score for reviews containing %s is %lf\n", word, avg);
         printf("\n");
+        revSum = revSum + avg;
+        
         strIn = nextWord;
         strIn++;
-        fclose(file);
-        file = fopen(fileName, "r");
+        fseek(file, 0, SEEK_SET);
     }
     
-    fclose(file);
 
+    fclose(file);
+    printf("The review has an average value of %lf\n", revSum/((double) numWord));
+    if((revSum/((double) numWord)) >= 2){
+    	printf("Positive Sentiment\n");
+    }
+
+    else{
+    	printf("Negative Sentiment\n");
+    }
     return 0;
 }
 
