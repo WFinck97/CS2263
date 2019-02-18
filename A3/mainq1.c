7057a6a75103c527a0f0ca4ca534e97fae71d4cc
Wesley Finck <wesleyfinck@gmail.com>
2019-02-17 21:26:10 -0400

    successfully read file and search each line

--- A3/mainq1.c
+++ A3/mainq1.c
@@ -14,24 +14,17 @@ int main(int argc, char* argv[]){
     char line[256];
 
     char word[MAX_STRING_LEN];
-    int *psum, *pnum, num, sum;
+    int *psum, *pnum, num = 0, sum = 0;
     psum = &sum;
     pnum = &num;
 
     printf("enter a word: ");
     scanf("%s", word);
 
-    printf("\n%s", word);
+    printf("%s\n", word);
 
-    //getWordStats(word, fileName, int *sum, int *num)
-    //while (fgets(line, sizeof(line), file)) {
-        /* note that fgets don't strip the terminating \n, checking its
-           presence would allow to handle lines longer that sizeof(line) */
-        //printf("%s", line); 
-    //}
-    /* may check feof here to make a difference between eof and io failure -- network
-       timeout for instance */
-
+    getWordStats(word, file, psum, pnum);
+    printf("%d\n", num);
     fclose(file);
 
     return 0;
