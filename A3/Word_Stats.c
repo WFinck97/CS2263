c610f36ddde14f5a7809e6a89d4557fd139eaa0e
Wesley Finck <wesleyfinck@gmail.com>
2019-02-17 20:26:50 -0400

    working on reading a file

--- A3/Word_Stats.c
+++ A3/Word_Stats.c
@@ -6,8 +6,16 @@ void getWordStats(char *word, FILE *f, int *sum, int *num){
 	/* look through each line for 'word', only need to look for the first instance
 	of the word. If found, add score to sum and num++. move to next line.
 	*/
-	char *line;
 
+	char line[256];
+
+    while (fgets(line, sizeof(line), file)) {
+        /* note that fgets don't strip the terminating \n, checking its
+           presence would allow to handle lines longer that sizeof(line) */
+        printf("%s", line); 
+    }
+    /* may check feof here to make a difference between eof and io failure -- network
+       timeout for instance */
 	if(strstr(line, word)){
 
 	}
--- A3/mainq1.c
+++ A3/mainq1.c
@@ -4,3 +4,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Word_Stats.h"
+
+int main(int argc, char* argv[]){
+	
+	char const* const fileName = argv[1]; /* should check that argc > 1 */
+    FILE* file = fopen(fileName, "r"); /* should check the result */
+    char line[256];
+
+    char *word;
+    int *psum, *pnum, num, sum;
+    psum = &sum;
+    pnum = &num;
+
+    getWordStats(word, fileName, int *sum, int *num)
+    while (fgets(line, sizeof(line), file)) {
+        /* note that fgets don't strip the terminating \n, checking its
+           presence would allow to handle lines longer that sizeof(line) */
+        printf("%s", line); 
+    }
+    /* may check feof here to make a difference between eof and io failure -- network
+       timeout for instance */
+
+    fclose(file);
+
+    return 0;
+}
