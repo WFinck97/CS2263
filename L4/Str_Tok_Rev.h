64d4b64f40699a9e1efaeba371ca4a099e45383d
Wesley Finck <wesleyfinck@gmail.com>
2019-03-06 13:15:45 -0400

    started adding to the makefile and .h

---
+++ L4/Str_Tok_Rev.h
@@ -0,0 +1,11 @@
+#ifndef Str_Tok_Rev_h
+#define Str_Tok_Rev_h
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/* description of the function here
+*/
+char **stringToTokens(char *str, char *sep);char *strlwr(char *str);
+#endif /* Str_Tok_Rev_h */
