f1f78c37456135c4affa3f2855660328e3f3aeae
Wesley Finck <wesleyfinck@gmail.com>
2019-03-06 14:24:15 -0400

    reading a sentence and counting the words

--- L4/Str_Tok_Rev.c
+++ L4/Str_Tok_Rev.c
@@ -1,7 +1,25 @@
 #include "Str_Tok_Rev.h"
 
 char **stringToTokens(char *str, char *sep){
+char *strpt = str, *wordToken;
+int numWords = 0;
+	
+	if((wordToken = strtok(strpt, " ")) != NULL){
+		numWords++;
+	}
+	else{
+		printf("only one word was entered");
+	}
+	printf("%s\n", wordToken);
 
+	while(wordToken != NULL){
+		wordToken = strtok(NULL, " ");
+		printf("%s\n", wordToken);
+		numWords++;
+	}
+
+	printf("number of words: %d\n", numWords);
+	return NULL;
 }
 
 void destroyTokens(char **tokenArray){
@@ -9,5 +27,5 @@ void destroyTokens(char **tokenArray){
 }
 
 void reverse(char *s){
-	
+
 }
--- L4/main.c
+++ L4/main.c
@@ -3,19 +3,24 @@
 #include <string.h>
 #include "Str_Tok_Rev.h"
 
-#define MAX_REVIEW_L 200
+#define MAX_SENTENCE_L 200
 #define LINEL 80
 #define WORDL 20
 
 int main(){
 
-	printf("enter a review:\n");
+	printf("enter sentence:\n");
     
-    char line[LINEL+1], word[WORDL+1];
+    char line[LINEL+1];
     fgets(line, LINEL+1, stdin);
     line[strlen(line)-1] = '\0';
 
-    char *strIn, *nextWord = line;
+    char *strIn;
 
     strIn = line;
+
+    printf("%s\n", strIn);
+
+    char **tokenArray = stringToTokens(strIn, ", ");
+    return 0;
 }
