836f14da2486868c1947dc5a4b2e3c942a1e112a
Wesley Finck <wesleyfinck@gmail.com>
2019-03-11 11:35:00 -0300

    adding descriptions to the header file

--- L4/Str_Tok_Rev.h
+++ L4/Str_Tok_Rev.h
@@ -5,15 +5,21 @@
 #include <stdio.h>
 #include <ctype.h>
 
-/* description of the function here
+/* stringToTokens takes a string and the separating characters. 
+It separates the string into tokens using the separator characters.
+An array of pointers is allocated on the heap with the size equivalent to
+the number of tokens. Then, a deep copy of each token is made and the copy
+is pointed to by the elements of the array of pointers. 
 */
 char **stringToTokens(char *str, char *sep);char *strlwr(char *str);
 
-/* description of the function here
+/* destryTokens takes the address of the array of pointers and frees 
+each deep copy as well as the array of pointers itself., removing them
+from the heap.
 */
 void destroyTokens(char **tokenArray);
 
-/* description of the function here
+/* reverse takes a string and reverses the elements of the string.
 */
 void reverse(char *s);
 
