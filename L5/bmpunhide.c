609bf4dcc5a3c6a4d718012f488ac3d987399a04
Wesley Finck <wesleyfinck@gmail.com>
2019-03-27 14:57:08 -0300

    brighten green after filtering out other colours

--- L5/bmpunhide.c
+++ L5/bmpunhide.c
@@ -3,6 +3,8 @@
 void BMP_unhide(BMP_Image *img){
 	int pxl;
 	unsigned char min = 0;
+	unsigned char threshold = 20;
+	unsigned char max = 255;
 	// keep only green pixels
 	for (pxl = 0; pxl < (img -> data_size); pxl += 3)
 	  {
@@ -10,5 +12,10 @@ void BMP_unhide(BMP_Image *img){
 	    img -> data[pxl + 2] = min;
 	    //get rid of blue pixels
 	    img -> data[pxl] = min;
+
+	    // if pixel is green, fully brighten it
+	    if(img -> data[pxl + 1] > threshold){
+	    	img -> data[pxl + 1] = max;
+	    }
 	  }
 }
