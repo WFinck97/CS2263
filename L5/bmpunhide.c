3e8b04b5e04f75b8226d1b9fa108f3bf3970a72a
Wesley Finck <wesleyfinck@gmail.com>
2019-03-27 14:46:50 -0300

    for loop that gets rid of blue and red

--- L5/bmpunhide.c
+++ L5/bmpunhide.c
@@ -2,17 +2,13 @@
 #include "bmpfunc.h"
 void BMP_unhide(BMP_Image *img){
 	int pxl;
-	
+	unsigned char min = 0;
+	// keep only green pixels
 	for (pxl = 0; pxl < (img -> data_size); pxl += 3)
 	  {
-	    unsigned char red   = img -> data[pxl + 2];
-	    unsigned char green = img -> data[pxl + 1];
-	    unsigned char blue  = img -> data[pxl];
-	    if (redmin > red) { redmin = red; }
-	    if (redmax < red) { redmax = red; }
-	    if (greenmin > green) { greenmin = green; }
-	    if (greenmax < green) { greenmax = green; }
-	    if (bluemin > blue) { bluemin = blue; }
-	    if (bluemax < blue) { bluemax = blue; }
+	    //get rid of red pixels
+	    img -> data[pxl + 2] = min;
+	    //get rid of blue pixels
+	    img -> data[pxl] = min;
 	  }
 }
