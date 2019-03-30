17c7251bfa12684e4a329dbc1bac7245aa162813
Wesley Finck <wesleyfinck@gmail.com>
2019-03-30 10:23:38 -0300

    created directory A5 and added code from D2L

---
+++ A5/graph.h
@@ -0,0 +1,27 @@
+//
+//  graph.h
+//  
+//
+//  Created by Gerhard Dueck on 2018-03-27.
+//
+
+#ifndef graph_h
+#define graph_h
+
+#include <stdio.h>
+
+typedef struct {
+    int v;
+    int w;
+    int wt;
+} Edge;
+
+/* GRAPHinit and GRAPHinsertE return 1 if successful
+ and return 0 if malloc fails */
+int GRAPHinit(int);
+int GRAPHinsertE(Edge);
+void GRAPHdestroy();
+int *SSSP(int);
+
+
+#endif /* graph_h */
---
+++ A5/main.c
@@ -0,0 +1,54 @@
+//
+//  main.c
+//  
+//
+//  Created by Gerhard Dueck on 2018-03-27.
+//
+
+
+/* Using Graph ADT to solve SSSP
+ * Requires graph input in edge list
+ */
+
+#include <stdio.h>
+#include "graph.h"
+
+int main(){
+    int n, m;
+    Edge e;
+    scanf("%d %d",&n, &m);
+    if(!GRAPHinit(n)){
+        printf("couldn't allocate memory for graph\n");
+        return 1;
+    }
+    for(int k=0; k<m; k++){
+        if(scanf("%d %d %d", &e.v, &e.w, &e.wt)!=3){
+            printf("input invalid\n");
+            return 1;
+        }
+        if(e.v > n-1 || e.w > n-1){
+            printf("vertex index too large\n");
+            return 1;
+        }
+        if(!GRAPHinsertE(e)){
+            printf("couldn't insert edge\n");
+            return 1;
+        }
+    }
+    
+    int s;
+    scanf("%d",&s);
+    if(s >= n || s < 0){
+        printf("invalid source vertex\n");
+        return 1;
+    }
+    int *D = SSSP(s);
+    if(D == NULL){
+        printf("SSSP malloc error\n");
+        return 1;
+    }
+    for(int i=0;i<n;i++)
+        printf("%d ",D[i]);
+    printf("\n");
+    GRAPHdestroy();
+}
