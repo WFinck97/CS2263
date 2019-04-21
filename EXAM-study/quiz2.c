cdc8c7f5b5c9db27894c19dd405ca321533d820e
Wesley Finck <wesleyfinck@gmail.com>
2019-04-21 11:40:04 -0300

    code for quiz 3 working on it rn

--- EXAM-study/quiz2.c
+++ EXAM-study/quiz2.c
@@ -5,6 +5,6 @@ int main(){
 	int a[] = {5, 25, 34, 54, 14, 22, 52, 91};
 	int *p = &a[1], *q = &a[5];
 
-	printf("q = %p, p = %p, q - p = %p\n", q,p,q-p);
+	printf("q = %p, p = %p, q - p = %ld\n", q,p,q-p);
 	return 0;
 }
---
+++ EXAM-study/quiz3.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+char * question2(char *s, int l);
+int isDigit(char *s);
+
+int main(int argc, char *argv[]){
+	// char str[] = "one two three", *str2;
+	// str2 = question2(str,3);
+	// printf("str = %s\n", str);
+	// printf("str2 = %s\n", str2);
+	// str2 = question2(str+8, 7);
+	// printf("str = %s\n", str);
+	// printf("str2 = %s\n", str2);
+	// return 0;
+
+	int product;
+	if(argc < 3){
+		printf("ERROR incorrect number of parameters\n");
+		return EXIT_FAILURE;
+	}
+
+	if(isDigit(argv[1])==0){
+		printf("ERROR first parameters must be a single digit\n");
+		return EXIT_FAILURE;
+	}
+
+	if(isDigit(argv[2])==0){
+		printf("ERROR second parameters must be a single digit\n");
+		return EXIT_FAILURE;
+	}
+
+	product = ((int)(*argv[1]-'0'))*((int)(*argv[2]-'0'));
+}
+
+char * question2(char *s, int l){
+	for(int i = 0; *s && i < l; i++){
+		*s = 'x';
+		s++;
+	}
+	return s;
+}
+
+int isDigit(char *s){
+	if(strlen(s) == 1 && s[0] >= '0' && s[0] <= '9')
+		return 1;
+	else
+		return 0;
+}
